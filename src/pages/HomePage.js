8164daa90bc1686e7832ec055a26ce5e5f35351c
TrietB <triet.bm1996@gmail.com>
2022-08-19 09:39:11 +0700

    update files

---
+++ src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import useAuth from "../hooks/useAuth"
+import { useNavigate } from "react-router-dom";
+
+function HomePage() {
+    const auth = useAuth()
+    let navigate = useNavigate()
+
+    if(!auth){
+        return <p>You are not logged in</p>
+    }
+  return (
+    <div>
+        <h1>Welcome{auth.user?.username}</h1>
+        <button onClick={()=>{
+            auth.logout(()=> navigate('/'))
+        }}>
+            Sign out
+        </button>
+    </div>
+  )
+}
+
+export default HomePage
