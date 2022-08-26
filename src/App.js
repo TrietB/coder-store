cd49c50118e2dd9ff72b01e0f32da0216a1d096f
TrietB <triet.bm1996@gmail.com>
2022-08-26 15:46:16 +0700

    remove unuse func/imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import { Typography } from '@mui/material'
-import { Box , Link } from '@mui/material'
+// import { Typography } from '@mui/material'
+// import {  Link } from '@mui/material'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './routes'
 import { AuthProvider } from './contexts/AuthContext'
 import ThemeProvider from './contexts/ThemeProvider'
 
-function CopyRight() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-    {'Copyright © '}
-    <Link color="inherit" href="https://www.coderschool.vn">
-      CoderSchool
-    </Link>{' '}
-    {new Date().getFullYear()}
-    {'.'}
-  </Typography>
-  )
-}
+// function CopyRight() {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center">
+//     {'Copyright © '}
+//     <Link color="inherit" href="https://www.coderschool.vn">
+//       CoderSchool
+//     </Link>{' '}
+//     {new Date().getFullYear()}
+//     {'.'}
+//   </Typography>
+//   )
+// }
 
 
 function App() {
